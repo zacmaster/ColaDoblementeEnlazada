e7c3881678991e6de5ee32b7b0e82cb466a49aa2
Zacarías Samaniego <zacariassamaniego@gmail.com>
2016-11-30 01:56:15 -0300

    Mas pruebas agregadas

--- tp2/src/tp2/parte1/Prueba.java
+++ tp2/src/tp2/parte1/Prueba.java
@@ -3,8 +3,16 @@ package tp2.parte1;
 public class Prueba {
 	public static void main(String[] args) {
 		DequeEnlazada<String> doblecola = new DequeEnlazada<String>();
-		System.out.println(doblecola.removeLast());
+		doblecola.add("A");
+		doblecola.add("B");
+		doblecola.add("C");
+		doblecola.add("D");
+		doblecola.add("E");
 		System.out.println(doblecola);
+		doblecola.removeLast();
+		doblecola.removeFirst();
+		System.out.println(doblecola);
+		System.out.println(doblecola.contains("Z"));
 	}
 
 }
